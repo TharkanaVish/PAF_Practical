0845ec13b48e9ae5a3ce50b0507f64dd04dd3f93
TharkanaVish <tharkana.vish@gmail.com>
2021-03-17 15:34:50 +0530

    finalizing update and delete methods

--- PAF_Lab/WebContent/items.jsp
+++ PAF_Lab/WebContent/items.jsp
@@ -3,45 +3,105 @@
     pageEncoding="ISO-8859-1" %>
     
 <%
-if (request.getParameter("itemCode") != null)
+if (request.getParameter("action") != null)
 { 
 	 Item itemObj = new Item();
-	 //itemObj.connect();//For testing the connect method
- 
-	 String stsMsg = itemObj.insertItem(request.getParameter("itemCode"), 
-			 request.getParameter("itemName"), 
-			 request.getParameter("itemPrice"), 
-			 request.getParameter("itemDesc"));
 	 
-	 session.setAttribute("statusMsg", stsMsg);
+	 //itemObj.connect();//For testing the connect method
+	 
+	 //call insert function
+	 if ( request.getParameter("action").equalsIgnoreCase("insert"))
+	 {
+		 String stsMsg = itemObj.insertItem(request.getParameter("itemCode"),
+				 request.getParameter("itemName"),
+				 request.getParameter("itemPrice"),
+				 request.getParameter("itemDesc"));
+ 		 session.setAttribute("statusMsg", stsMsg);
+ 	 }
+	 
+	 //call update function
+	 else if (request.getParameter("action").equalsIgnoreCase("update"))
+	 {
+		System.out.println("newUpdate");
+	    String stsMsg = itemObj.updateItem(Integer.parseInt(request.getParameter("itemID")),
+	    		request.getParameter("itemCode"),
+	    		request.getParameter("itemName"),
+	    		request.getParameter("itemPrice"),
+	    		request.getParameter("itemDesc"));
+	 	session.setAttribute("statusMsg", stsMsg);
+	 }
+	 
+	 //call delete function
+	 else if(request.getParameter("action").equalsIgnoreCase("delete"))
+	 {
+		System.out.println("newDelete");
+	 	String stsMsg = itemObj.deleteItem(Integer.parseInt(request.getParameter("itemID")));
+	    session.setAttribute("statusMsg", stsMsg);
+	 }
+ 
+
 }	
  %>
 
 <!DOCTYPE html>
 <html>
 <head>
+
 <meta charset="ISO-8859-1">
 <title>Items Management</title>
 
-</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<div class="container">
+      <div class="row">
+           <div class="col">
+ 
+            </div>
+     </div>
+</div>
+
 <body>
 	<h1>Items Management</h1>
-	
-	<form method="post" action="items.jsp">
-	 Item Code: <input name="itemCode" type="text"><br> 
-	 Item Name: <input name="itemName" type="text"><br> 
-	 Item Price: <input name="itemPrice" type="text"><br> 
-	 Item Description: <input name="itemDesc" type="text"><br> 
-	 <input name="btnSubmit" type="submit" value="Save">
-	</form>
+	<br>
+
+	<%
+	if (request.getParameter("action") != null) {
+		//call the select function
+		if (request.getParameter("action").toString().equalsIgnoreCase("select")) {
+			Item itemObj = new Item();
+			out.print(itemObj.readOneItem(Integer.parseInt(request.getParameter("itemID"))));
+		} 
+		else {
+			out.print("<form method='post' action='items.jsp'> " + "<input name='action' value='insert' type='hidden'> "
+			+ "Item code: <input name='itemCode' type='text'><br>"
+			+ "Item name: <input name='itemName' type='text'><br> "
+			+ "Item price: <input name='itemPrice' type='text'><br> "
+			+ "Item description: <input name='itemDesc' type='text'><br> "
+			+ "<input name='btnSubmit' type='submit' value='Save'> " + "</form>");
+		}
+	}
+	else {
+		out.print("<form method='post' action='items.jsp'> " + "<input name='action' value='insert' type='hidden'> "
+		+ "Item code: <input name='itemCode' type='text'><br>"
+		+ "Item name: <input name='itemName' type='text'><br> "
+		+ "Item price: <input name='itemPrice' type='text'><br> "
+		+ "Item description: <input name='itemDesc' type='text'><br> "
+		+ "<input name='btnSubmit' type='submit' value='Save'> " + "</form>");
+	}
+	%>
+	<br>
+	<h3><%
+ 	out.print(session.getAttribute("statusMsg"));
+	%></h3>
 	
 	<br>
-	<h2><% out.print(session.getAttribute("statusMsg")); %></h2>
 	
 	<br>
 	<%
-	 Item itemObj = new Item(); 
-	 out.print(itemObj.readItems()); 
+ 	Item itemObj = new Item();
+ 	out.print(itemObj.readItems());
 	%>
 </body>	
 </html>
+
+
--- PAF_Lab/src/com/Item.java
+++ PAF_Lab/src/com/Item.java
@@ -75,7 +75,7 @@ public class Item {
 		return output;
 		}
 	
-	//read option method
+	
 	//read option method
 	public String readItems()
 	{ 
@@ -118,13 +118,16 @@ public class Item {
 			 output += "<td>" + itemDesc + "</td>"; 
 			
 			 // buttons
-			 output += "<td><input name='btnUpdate' "
-			 		+ "type='button' value='Update'></td>"
-			 		+ "<td><form method='post' action='items.jsp'>"
-			 		+ "<input name='btnRemove' "
-			 		+ "type='submit' value='Remove'>"
-			 		+ "<input name='itemID' type='hidden' "
-			 		+ "value='" + itemID + "'></form></td></tr>";
+			 output += "<td><form method='post' action='items.jsp'>"
+						+"<input name='itemID' type='hidden' value='" + itemID + "'>"
+						+ "<input name='action' value='select' type='hidden'>"
+						+ "<input name='btnUpdate' type='submit' value='Update'>"
+						+ "</form></td>"
+						+ "<td><form method='post' action='items.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove'>"
+						+ "<input name='action' value='delete' type='hidden'>"
+						+ "<input name='itemID' type='hidden' value='" + itemID + "'>" 
+						+ "</form></td></tr>";
 		}
 		
 		con.close();
@@ -138,6 +141,127 @@ public class Item {
 		 output = "Error while reading the items."; 
 		 System.err.println(e.getMessage()); 
 	 }
+	 return output;
+	 
+	}
+	
+	
+	//get one row details to update
+	public String readOneItem(int id)
+	{
+		//test the id
+		//System.out.print(id);
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				return "Error while connecting to the database for selected item reading.";
+			}
+	 
+			String query = "select * from items where itemID = ? ";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			preparedStmt.setInt(1, id);
+			ResultSet rs = preparedStmt.executeQuery();
+			// iterate through the rows in the result set
+			while (rs.next())
+			{
+				String itemID = Integer.toString(rs.getInt("itemID"));
+				String itemCode = rs.getString("itemCode");
+				String itemName = rs.getString("itemName");
+				String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+				String itemDesc = rs.getString("itemDesc");
+				output += "<form method=post action=items.jsp>"
+						+ "		<input name='action' value='update' type='hidden'>"
+						+ " 	Item ID: '"+itemID+"' <br>"
+						+ " 	Item code: <input name=itemCode type=text value='"+itemCode+ "'><br>"
+						+ " 	Item name: <input name=itemName type=text value='"+itemName+ "'><br>"
+						+ " 	Item price: <input name=itemPrice type=text value='"+itemPrice+ "'><br>"
+						+ " 	Item description: <input name=itemDesc type=text value='"+itemDesc+ "'><br>"
+						+ "     <input name='itemID' type='hidden' value='" + itemID + "'>"
+						+ " 	<input name=btnSubmit type=submit value=Update >"
+						+ "	</form>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+	 
+	}
+	catch (Exception e)
+	{
+		output = "Error while reading the one item.";
+		System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	
+	//update method
+	public String updateItem(int iID,String iCode,String iName,String iPrice, String iDesc) {
+		String output = "";
+		
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				return "Error while connecting to the database";
+			}
+			// create a prepared statement      
+			String query = "update items set itemCode=?,itemName =?,itemPrice=?,itemDesc=? where itemID = ?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setString(1, iCode);
+			preparedStmt.setString(2, iName);
+			preparedStmt.setDouble(3,Double.parseDouble(iPrice));
+			preparedStmt.setString(4, iDesc);
+			preparedStmt.setInt(5, iID);
+			
+			//execute the statement
+			preparedStmt.executeUpdate();
+			con.close();
+			output = "Updated successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while updating a one item";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	//delete method
+	public String deleteItem(int itemID)
+	{ 
+	 String output = "";
+	 
+	 try
+	 { 
+		 Connection con = connect(); 
+		 if (con == null) { 
+			 return "Error while connecting to the database for deleting."; 
+		 }
+		 
+		 // create a prepared statement
+		 String query = "delete from items where itemID=?"; 
+		 PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 
+		 // binding values
+		 preparedStmt.setInt(1, itemID); 
+		  
+		 // execute the statement
+		 preparedStmt.execute(); 
+		 con.close();
+		 
+		 output = "Deleted successfully";
+	 } 
+	 catch (Exception e) 
+	 { 
+		 output = "Error while deleting the item."; 
+		 System.err.println(e.getMessage()); 
+	 }
+	 
 	 return output; 
 	}
 
